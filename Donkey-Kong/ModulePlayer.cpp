9d97822b0479fe78027f199ba48c059e13c48680
FeroXx07 <ali.shahid.1212@gmail.com>
2020-03-31 18:52:50 +0200

    Initial Configuration Imported from Modules

---
+++ Donkey-Kong/ModulePlayer.cpp
@@ -0,0 +1,128 @@
+#include "ModulePlayer.h"
+
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleInput.h"
+#include "ModuleRender.h"
+
+#include "ModuleAudio.h"
+
+
+#include "SDL/include/SDL_scancode.h"
+
+
+ModulePlayer::ModulePlayer()
+{
+	// idle animation - just one sprite
+	idleAnim.PushBack({ 66, 1, 32, 14 });
+
+	// move upwards
+	upAnim.PushBack({ 100, 1, 32, 14 });
+	upAnim.PushBack({ 132, 0, 32, 14 });
+	upAnim.loop = false;
+	upAnim.speed = 0.1f;
+
+	// Move down
+	downAnim.PushBack({ 33, 1, 32, 14 });
+	downAnim.PushBack({ 0, 1, 32, 14 });
+	downAnim.loop = false;
+	downAnim.speed = 0.1f;
+}
+
+ModulePlayer::~ModulePlayer()
+{
+
+}
+
+bool ModulePlayer::Start()
+{
+	LOG("Loading player textures");
+
+	bool ret = true;
+
+	texture = App->textures->Load("Assets/ship.png"); // arcade version
+	currentAnimation = &idleAnim;
+
+	laserFx = App->audio->LoadFx("Assets/laser.wav");
+	explosionFx = App->audio->LoadFx("Assets/explosion.wav");
+
+	position.x = 150;
+	position.y = 120;
+
+	// TODO 3: Add a collider to the player
+
+	return ret;
+}
+
+update_status ModulePlayer::Update()
+{
+	// Moving the player with the camera scroll
+	App->player->position.x += 1;
+
+	if (App->input->keys[SDL_SCANCODE_A] == KEY_STATE::KEY_REPEAT)
+	{
+		position.x -= speed;
+	}
+
+	if (App->input->keys[SDL_SCANCODE_D] == KEY_STATE::KEY_REPEAT)
+	{
+		position.x += speed;
+	}
+
+	if (App->input->keys[SDL_SCANCODE_S] == KEY_STATE::KEY_REPEAT)
+	{
+		position.y += speed;
+		if (currentAnimation != &downAnim)
+		{
+			downAnim.Reset();
+			currentAnimation = &downAnim;
+		}
+	}
+
+	if (App->input->keys[SDL_SCANCODE_W] == KEY_STATE::KEY_REPEAT)
+	{
+		position.y -= speed;
+		if (currentAnimation != &upAnim)
+		{
+			upAnim.Reset();
+			currentAnimation = &upAnim;
+		}
+	}
+
+
+
+	// If no up/down movement detected, set the current animation back to idle
+	if (App->input->keys[SDL_SCANCODE_S] == KEY_STATE::KEY_IDLE
+		&& App->input->keys[SDL_SCANCODE_W] == KEY_STATE::KEY_IDLE)
+		currentAnimation = &idleAnim;
+
+	// TODO 4: Update collider position to player position
+
+	currentAnimation->Update();
+
+	if (destroyed)
+	{
+		destroyedCountdown--;
+		if (destroyedCountdown <= 0)
+			return update_status::UPDATE_STOP;
+	}
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+update_status ModulePlayer::PostUpdate()
+{
+	if (!destroyed)
+	{
+		SDL_Rect rect = currentAnimation->GetCurrentFrame();
+		App->render->Blit(texture, position.x, position.y, &rect);
+	}
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+void ModulePlayer::OnCollision(Collider* c1, Collider* c2)
+{
+	// TODO 5: Detect collision with a wall. If so, destroy the player.
+
+}
