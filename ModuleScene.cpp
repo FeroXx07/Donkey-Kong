688d361119155ca5378229a9371eddb8b3241510
Montuuh <dmontufo@gmail.com>
2020-04-08 19:17:26 +0200

    Level 4
    Player and ground colliders

--- ModuleScene.cpp
+++ ModuleScene.cpp
@@ -16,7 +16,10 @@ ModuleScene::ModuleScene()
 
 
 	//Level 4 scene sprite
-
+	level_4.x = 472;
+	level_4.y = 160;
+	level_4.w = SCREEN_WIDTH;
+	level_4.h = SCREEN_HEIGHT;
 }
 
 ModuleScene::~ModuleScene()
@@ -31,10 +34,11 @@ bool ModuleScene::Start()
 
 	bool ret = true;
 	//Scene sprites
-	bgTexture = App->textures->Load("Assets/Background.png");
+	bgTexture = App->textures->Load("Assets/Background2.png");
 	App->audio->PlayMusic("Assets/stage1.ogg", 1.0f);
 
-	/*LEVEL 2 COLLIDERS
+	// Level 2 colliders:
+	/*
 	App->collisions->AddCollider({ 0, 248, 224, 8 }, Collider::Type::GROUND);
 	App->collisions->AddCollider({ 8, 208, 208, 8 }, Collider::Type::GROUND);
 
@@ -52,7 +56,24 @@ bool ModuleScene::Start()
 	//Add collider for the wall
 	App->collisions->AddCollider({ 96, 144, 32, 24 }, Collider::Type::WALL);*/
 
+	// Level 4 colliders:
+	App->collisions->AddCollider({ 0, 248, 224, 8 }, Collider::Type::GROUND); // Base
 
+	App->collisions->AddCollider({ 8, 208, 48, 8 }, Collider::Type::GROUND); // Floor 1
+	App->collisions->AddCollider({ 64, 208, 96, 8 }, Collider::Type::GROUND); // Floor 1
+	App->collisions->AddCollider({ 168, 208, 48, 8 }, Collider::Type::GROUND); // Floor 1
+
+	App->collisions->AddCollider({ 16, 168, 40, 8 }, Collider::Type::GROUND); // Floor 2
+	App->collisions->AddCollider({ 64, 168, 96, 8 }, Collider::Type::GROUND); // Floor 2
+	App->collisions->AddCollider({ 168, 168, 40, 8 }, Collider::Type::GROUND); // Floor 2
+
+	App->collisions->AddCollider({ 24, 128, 32, 8 }, Collider::Type::GROUND); // Floor 3
+	App->collisions->AddCollider({ 64, 128, 96, 8 }, Collider::Type::GROUND); // Floor 3
+	App->collisions->AddCollider({ 168, 128, 32, 8 }, Collider::Type::GROUND); // Floor 3
+
+	App->collisions->AddCollider({ 32, 88, 24, 8 }, Collider::Type::GROUND); // Floor 4
+	App->collisions->AddCollider({ 64, 88, 96, 8 }, Collider::Type::GROUND); // Floor 4
+	App->collisions->AddCollider({ 168, 88, 24, 8 }, Collider::Type::GROUND); // Floor 4
 
 	return ret;
 }
